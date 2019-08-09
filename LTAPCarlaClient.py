2d2791042828bba1cfcd5fc92aaaad4e4a54e772
Arkady Zgonnikov <cherepaha@users.noreply.github.com>
2019-08-09 13:05:50 +0200

    Add files via upload

---
+++ LTAPCarlaClient.py
@@ -0,0 +1,278 @@
+from pathlib import Path
+import glob
+import os
+import sys
+from exp_info_ui import ExpInfoUI
+
+try:
+    sys.path.append(glob.glob(os.path.join(str(Path.home()),
+    'CarlaLatest/PythonAPI/carla/dist/carla-*%d.%d-%s.egg' % (
+            sys.version_info.major,
+            sys.version_info.minor,
+            'win-amd64' if os.name == 'nt' else 'linux-x86_64')))[0])
+except IndexError:
+    pass
+
+import carla
+import pygame
+import math
+import time
+import random
+import numpy as np
+import tkinter as tkr
+import csv
+
+class LTAPCarlaClient():
+    block_size = 150
+    lane_width = 3.5
+
+    bot_blueprint_names = ['vehicle.tesla.model3', 'vehicle.nissan.micra', 'vehicle.volkswagen.t2',
+                           'vehicle.mini.cooperst', 'vehicle.citroen.c3', 'vehicle.tesla.model3',
+                           'vehicle.dodge_charger.police']
+
+    def __init__(self):
+        try:
+            self.exp_info = self.get_exp_info()
+            self.n_routes = 1
+
+            self.initialize_log()
+            self.client = carla.Client('localhost', 2000)
+            self.client.set_timeout(2.0)
+            pygame.init()
+            self.world = self.client.get_world()
+
+            self.joystick = pygame.joystick.Joystick(0)
+            self.joystick.init()
+            print('Joystick ID: %d Init status: %s' % (self.joystick.get_id(), self.joystick.get_init()))
+
+            # x and y indices of the intersection
+            # (0, 0) is the intersection in the bottom left corner of the map
+            # (4, 4) is the intersection in the top right corner of the map
+            self.origin = np.array([0.0, 0.0])
+
+            # (1,0) is east, (-1,0) is west, (0,1) is north, (0,-1) is south
+            self.active_intersection = np.array([1.0, 0.0])
+
+            self.sound_cues = {(1, 1): 'next_turn_left',
+                               (1, 0): 'next_go_straight',
+                               (1, -1): 'next_turn_right',
+                               (2, 1): 'turn_left',
+                               (2, 0): 'go_straight',
+                               (2, -1): 'turn_right'}
+            '''
+            To shift the starting position from the center of the intersection to the lane where
+            the driver can start driving towards the first active intersection, we rotate the
+            heading direction 90` clockwise (-np.pi/2), and shift the origin towards that direction by half lane width
+            '''
+            start_position = self.origin*self.block_size + \
+                        self.rotate(self.active_intersection-self.origin, -np.pi/2)*self.lane_width/2
+
+            # whenever we send or get y-coordinated to carla, we should invert the y-coordinate
+            self.ego_start_position = self.world.get_map().get_waypoint(
+                carla.Location(x=start_position[0], y=-start_position[1], z=0))
+
+            ego_bp = random.choice(self.world.get_blueprint_library().filter('vehicle.tesla.model3'))
+
+            self.ego_actor = self.world.spawn_actor(ego_bp, self.ego_start_position.transform)
+            self.ego_actor.set_autopilot(False)
+
+            self.bot_actor = None
+            self.bot_actor_blueprints = [random.choice(self.world.get_blueprint_library().filter(bp_name))
+                                            for bp_name in self.bot_blueprint_names]
+
+            self.empty_control = carla.VehicleControl(
+                throttle = 0,
+                steer = 0,
+                brake = 0,
+                hand_brake = False,
+                reverse = False,
+                manual_gear_shift = False,
+                gear = 0)
+            self.control = self.empty_control
+
+        except KeyboardInterrupt:
+            for actor in self.world.get_actors():
+                actor.destroy()
+
+    def initialize_log(self):
+        log_directory = 'data'
+        self.log_file_path = os.path.join(log_directory, str(self.exp_info['subj_id']) + '_' +
+                                                         str(self.exp_info['session']) + '_' +
+                                                         self.exp_info['start_time'] + '.txt')
+        with open(self.log_file_path, 'w') as fp:
+            writer = csv.writer(fp, delimiter = '\t')
+            writer.writerow(['subj_id', 'session', 'route', 'intersection', 'turn_direction', 'timestamp',
+                             'ego_x', 'ego_y', 'ego_vx', 'ego_vy', 'ego_ax', 'ego_ay', 'ego_yaw',
+                             'bot_x', 'bot_y', 'bot_vx', 'bot_vy', 'bot_ax', 'bot_ay', 'bot_yaw',
+                             'throttle', 'brake', 'steer'])
+
+    def write_log(self, log):
+        with open(self.log_file_path, 'a') as fp:
+            writer = csv.writer(fp, delimiter = '\t', )
+            writer.writerows(log)
+
+    def get_exp_info(self):
+        root = tkr.Tk()
+        app = ExpInfoUI(master=root)
+        app.mainloop()
+        exp_info = app.exp_info
+        root.destroy()
+
+        return exp_info
+
+    def rotate(self, vector, angle):
+        c, s = np.cos(angle), np.sin(angle)
+        return np.squeeze(np.asarray(np.dot(np.matrix([[c, -s], [s, c]]), vector)))
+
+    def update_control(self):
+        reverse = False
+        K1 = 1.0  # 0.55
+        steerCmd = K1 * math.tan(1.1 * self.joystick.get_axis(0))
+
+        K2 = 1.6  # 1.6
+        throttleCmd = K2 + (2.05 * math.log10(
+            -0.7 * self.joystick.get_axis(2) + 1.4) - 1.2) / 0.92
+        if throttleCmd <= 0:
+            throttleCmd = 0
+        elif throttleCmd > 1:
+            throttleCmd = 1
+
+        brakeCmd = 1.6 + (2.05 * math.log10(
+            -0.7 * self.joystick.get_axis(3) + 1.4) - 1.2) / 0.92
+        if brakeCmd <= 0:
+            brakeCmd = 0
+        elif brakeCmd > 1:
+            brakeCmd = 1
+
+        if self.joystick.get_button(5):
+            reverse = True
+        elif self.joystick.get_button(4):
+            reverse = False
+
+        for event in pygame.event.get():
+            if event.type == pygame.KEYUP:
+                if event.key == pygame.locals.K_ESCAPE:
+                    raise KeyboardInterrupt
+
+        self.control.throttle = throttleCmd
+        self.control.steer = steerCmd
+        self.control.brake = brakeCmd
+        self.control.reverse = reverse
+
+
+    def spawn_bot(self, distance_to_intersection, speed):
+        # TODO: make sure that the bots are regular-sized cars with bright colors
+        bot_bp = random.choice(self.world.get_blueprint_library().filter('vehicle.*'))
+
+        ego_direction = self.active_intersection - self.origin
+        spawn_location = self.active_intersection*self.block_size + \
+                            distance_to_intersection*(ego_direction) + \
+                            (self.lane_width/2) * self.rotate(ego_direction, np.pi/2)
+        spawn_waypoint = self.world.get_map().get_waypoint(
+                        carla.Location(x=spawn_location[0], y=-spawn_location[1], z=0))
+
+        self.bot_actor = self.world.spawn_actor(bot_bp, spawn_waypoint.transform)
+
+        bot_velocity = speed*self.rotate(ego_direction, np.pi)
+        self.bot_actor.set_velocity(carla.Vector3D(bot_velocity[0], -bot_velocity[1], 0))
+
+    def play_sound_cue(self, number, direction):
+        sound_filename = '%s.wav' % (self.sound_cues[(number, direction)])
+        file_path = os.path.join('sounds', sound_filename)
+        sound = pygame.mixer.Sound(file_path)
+        sound.play()
+
+    def get_actor_state(self, actor):
+        state = ([actor.get_transform().location.x, -actor.get_transform().location.y,
+                 actor.get_velocity().x, -actor.get_velocity().y,
+                 actor.get_acceleration().x, -actor.get_acceleration().y,
+                 actor.get_transform().rotation.yaw]
+                 if not (actor is None) else np.zeros(7).tolist())
+        return list(['%.4f' % value for value in state])
+
+    def update_log(self, log, timestamp, route, intersection, current_turn):
+        log.append(([self.exp_info['subj_id'], self.exp_info['session'], route, intersection, current_turn, timestamp] + \
+                    self.get_actor_state(self.ego_actor) + \
+                    self.get_actor_state(self.bot_actor) + \
+                    list(['%.4f' % value for value in [self.control.throttle, self.control.brake, self.control.steer]])))
+
+    def run(self):
+        try:
+            print(self.exp_info)
+            for i in range(self.n_routes):
+                # in the path input file, -1 is turn right, 1 is turn left, 0 is go straight
+                path = np.loadtxt('path_%i.txt' % (i+1))
+                for j, current_turn in enumerate(path):
+                    is_turn_completed = False
+                    is_at_active_intersection = False
+                    is_first_cue_played = False
+                    is_second_cue_played = False
+
+                    active_intersection_loc = carla.Location(x=self.active_intersection[0]*self.block_size,
+                                                             y=-self.active_intersection[1]*self.block_size,
+                                                             z=0.0)
+                    trial_log = []
+                    trial_start_time = time.time()
+
+                    while not is_turn_completed:
+                        speed = np.sqrt(self.ego_actor.get_velocity().x**2 + self.ego_actor.get_velocity().y**2)
+                        distance_to_active_intersection = self.ego_actor.get_location().distance(active_intersection_loc)
+
+                        print(i, j, is_at_active_intersection, self.origin, self.active_intersection,
+                              current_turn, '%.3f %.3f'% (distance_to_active_intersection, speed))
+
+                        self.update_log(trial_log, time.time()-trial_start_time, i+1, j+1, current_turn)
+
+                        self.update_control()
+                        self.ego_actor.apply_control(self.control)
+
+                        if((not is_first_cue_played) & (self.ego_actor.get_location().distance(active_intersection_loc)<(4/5)*self.block_size)):
+                            self.play_sound_cue(1, current_turn)
+                            is_first_cue_played = True
+                        elif((not is_second_cue_played) & (self.ego_actor.get_location().distance(active_intersection_loc)<(2/5)*self.block_size)):
+                            self.play_sound_cue(2, current_turn)
+                            is_second_cue_played = True
+                        # When the driver enters the intersection, we destroy the bot
+                        elif((not is_at_active_intersection) & (self.ego_actor.get_location().distance(active_intersection_loc)<10)):
+                            is_at_active_intersection = True
+                            if (not (self.bot_actor is None)):
+                                self.bot_actor.destroy()
+                                self.bot_actor = None
+                        elif((current_turn==1) & (is_at_active_intersection) & (speed<2) & (self.bot_actor is None)):
+                            self.spawn_bot(75, 20)
+                        elif((current_turn==-1) & (is_at_active_intersection) & (self.bot_actor is None)):
+                            self.spawn_bot(75, 20)
+                        # When the driver leaves the intersection we designate the next intersection as active
+                        elif((is_at_active_intersection) & (self.ego_actor.get_location().distance(active_intersection_loc)>10)):
+                            current_direction = self.active_intersection - self.origin
+                            new_origin = self.active_intersection
+                            new_active_intersection = self.active_intersection + self.rotate(current_direction, np.pi/2*current_turn)
+                            self.origin = new_origin
+                            self.active_intersection = new_active_intersection
+
+                            is_turn_completed = True
+
+                        time.sleep(0.01)
+
+                    self.write_log(trial_log)
+
+                self.ego_actor.apply_control(self.empty_control)
+                self.ego_actor.set_transform(self.ego_start_position.transform)
+                self.ego_actor.set_velocity(carla.Vector3D(0,0,0))
+                time.sleep(5.0)
+
+            for actor in self.world.get_actors():
+                actor.destroy()
+
+        except KeyboardInterrupt:
+            for actor in self.world.get_actors():
+                actor.destroy()
+
+def main():
+    ltap_carla_client = LTAPCarlaClient()
+    ltap_carla_client.run()
+
+if __name__ == '__main__':
+    main()
+
+
