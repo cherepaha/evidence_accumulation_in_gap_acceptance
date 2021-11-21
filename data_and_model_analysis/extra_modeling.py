3a9d09244280ee9988ebbee0f497a2955b0e4447
zi <zgoirina@gmail.com>
2021-11-22 00:48:37 +0100

    Figures for model selection added

--- data_and_model_analysis/extra_modeling.py
+++
@@ -1,8 +0,0 @@
-import models
-import pandas as pd
-import helper
-
-modelTtaBounds = models.ModelTtaBounds()
-exp_data = pd.read_csv('../data/measures.csv', usecols=['subj_id', 'RT', 'is_turn_decision',
-                                                    'tta_condition', 'd_condition'])
-subjects = exp_data.subj_id.unique()
